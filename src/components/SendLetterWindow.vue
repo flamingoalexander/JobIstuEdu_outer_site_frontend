a18556deae7b42bc287cdd55a00cce47e5ac2fd8
Alexander Vokin <setvvgamer@gmail.com>
2024-11-25 12:31:36 +0800

    add mailto link and document download link

--- src/components/SendLetterWindow.vue
+++ src/components/SendLetterWindow.vue
@@ -1,10 +1,21 @@
 <template>
     <div>
-        <p>Для регистрации вашей компании необходимо отправить заполненную версию документа на адрес job.istu.edu@istu.ru</p>
+        <p>Для регистрации вашей компании необходимо отправить заполненную версию
+            <a href="https://job.istu.edu/out/templates" >документа</a>
+                на адрес  <a :href="emailLink" class="email-link">job.istu.edu@istu.ru</a></p>
     </div>
 </template>
 
 <script>
+export default {
+    setup() {
+        const emailLink = 'mailto:job.istu.edu@istu.ru?subject=Регистрация компании&body=Заполненная версия документа';
+
+        return {
+            emailLink,
+        };
+    },
+};
 </script>
 
 <style scoped>
