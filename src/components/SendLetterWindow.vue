0b5f460feddceed4af5ffbd011bdb7e5d48d7446
Alexander Vokin <setvvgamer@gmail.com>
2024-11-25 12:18:19 +0800

    add third button in hat and create SendLetter component

---
+++ src/components/SendLetterWindow.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>Для регистрации вашей компании необходимо отправить заполненную версию документа на адрес job.istu.edu@istu.ru</p>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+p {
+    font-size: 18px;
+    color: #333;
+}
+
+</style>
---
+++ src/views/SendLetterWindowView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import SendLetterWindow from '@/components/SendLetterWindow.vue';
+</script>
+
+<template>
+    <SendLetterWindow/>
+</template>
+
+<style scoped>
+
+</style>
