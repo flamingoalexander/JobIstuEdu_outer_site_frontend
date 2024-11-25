ae1fb3a5924f9bf7a20edba184388d3843e9f5a8
Alexander Vokin <setvvgamer@gmail.com>
2024-11-25 13:02:43 +0800

    add styles

--- src/components/Hat.vue
+++ src/components/Hat.vue
@@ -11,12 +11,11 @@
 
         </div>
         <div class="icons">
-            <RouterLink :to="{ name: 'SendLetter' }"><button class="hat_butt"><img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRALBplMxySL51Y_dJZ5FTrLpa1rpD72FebJA&usqp=CAU"
-                style="height: 35px; width: 35px; margin-left: 7px;">
-
-
-            </button></RouterLink>
+            <RouterLink :to="{ name: 'SendLetter' }">
+                <button class="btn">
+                    Регистрация
+                </button>
+            </RouterLink>
             <a href="https://t.me/ForStudentIRNITUbot">
                 <button class="hat_butt"><img
                         src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/1200px-Telegram_logo.svg.png"
--- src/components/SendLetterWindow.vue
+++ src/components/SendLetterWindow.vue
@@ -1,8 +1,11 @@
 <template>
-    <div>
-        <p>Для регистрации вашей компании необходимо отправить заполненную версию
-            <a href="https://job.istu.edu/out/templates" >документа</a>
-                на адрес  <a :href="emailLink" class="email-link">job.istu.edu@istu.ru</a></p>
+    <div class="message-container">
+        <p class="message">
+            Для регистрации вашей компании необходимо отправить заполненную версию
+            <a href="https://job.istu.edu/out/templates" class="document-link">документа</a>
+            на адрес
+            <a :href="emailLink" class="email-link">job.istu.edu@istu.ru</a>
+        </p>
     </div>
 </template>
 
@@ -19,9 +22,39 @@ export default {
 </script>
 
 <style scoped>
-p {
-    font-size: 18px;
-    color: #333;
+/* Оформляем контейнер */
+.message-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
+/* Стили для текста */
+.message {
+    font-size: 18px;
+    color: #333;
+    background-color: #ffffff; /* Белый фон для блока с текстом */
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    max-width: 600px; /* Ограничиваем максимальную ширину */
+    text-align: center;
+}
+
+/* Стили для ссылок */
+.document-link {
+    text-decoration: none;
+}
+
+.document-link:hover {
+    text-decoration: underline;
+}
+
+.email-link {
+    text-decoration: none;
+}
+
+.email-link:hover {
+    text-decoration: underline;
+}
 </style>
