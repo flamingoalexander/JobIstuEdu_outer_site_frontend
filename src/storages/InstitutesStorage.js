9e087facc0fb9572e8827a4e337270fcc125bc92
Alexander Vokin <setvvgamer@gmail.com>
2024-12-02 20:00:51 +0800

    bug fix with reload page

--- src/storages/InstitutesStorage.js
+++ src/storages/InstitutesStorage.js
@@ -3,13 +3,16 @@ import { defineStore } from "pinia";
 export const useInstitutesStorage = defineStore("InstitutesStorage", {
   state: () => ({
     institutes: [],
+    isEmpty: true,
   }),
   actions: {
     async getInstitutesFromServer() {
-      axios.get("/api/out/legacy/faculty")
+      this.isEmpty = true
+      await axios.get("/api/out/legacy/faculty")
           .then((response) => {
             this.institutes = response.data;
           });
+      this.isEmpty = false;
     },
     getInstituteById(id){
       return this.institutes.find((institute) => institute.id === id);
--- src/views/PracticeView.vue
+++ src/views/PracticeView.vue
@@ -1,10 +1,16 @@
 <template>
     <div>
-        <div class="container-xl">
+        <div v-if="!isLoading" class="container-xl">
             <InstituteInfo :instId="instituteId" />
         </div>
-        <div class="container-xl">
+        <div v-else>
+            <h1>LOADING...</h1>
+        </div>
+        <div v-if="!isLoading" class="container-xl">
             <PracticeList :instId="instituteId"/>
+        </div>
+        <div v-else>
+            <h1>LOADING...</h1>
         </div>
 
     </div>
@@ -14,7 +20,7 @@ import PracticeList from '@/components/PracticeList.vue';
 import InstituteInfo from '@/components/InstituteInfo.vue';
 import {useInstitutesStorage} from "@/storages/InstitutesStorage";
 import {usePartnersStorage} from "@/storages/PartnersStorage";
-import {onBeforeMount} from "vue";
+import {onBeforeMount, ref} from "vue";
 const InstitutesStorage = useInstitutesStorage()
 const PartnersStorage = usePartnersStorage()
 const props = defineProps({
@@ -22,10 +28,17 @@ const props = defineProps({
         required: true,
     }
 });
-
-onBeforeMount(()=>{
-    if(InstitutesStorage.institutes.length === 0){
-        InstitutesStorage.getInstitutesFromServer()
+const isLoading = ref(false)
+onBeforeMount(async ()=>{
+    if (InstitutesStorage.isEmpty === true) {
+        isLoading.value = true;
+        try {
+            await InstitutesStorage.getInstitutesFromServer()
+        } catch (error) {
+            console.log(error)
+        } finally {
+            isLoading.value = false;
+        }
     }
 })
 </script>
