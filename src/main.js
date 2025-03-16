1abbb664ae39d4f5b53aa98a1250946e00866afc
Alexander Vokin <setvvgamer@gmail.com>
2025-03-16 12:38:19 +0800

    install elementPlus

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/main.css'
 import router from './router'
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
+import { createPinia } from 'pinia'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import "bootstrap"
 
- import { createPinia } from 'pinia'
- import 'bootstrap/dist/css/bootstrap.css'
- import 'bootstrap-vue/dist/bootstrap-vue.css'
- import 'bootstrap-icons/font/bootstrap-icons.css'
- import "bootstrap"
-
-createApp(App).use(router).use(createPinia()).mount('#app')
+createApp(App).use(router).use(createPinia()).use(ElementPlus).mount('#app')
